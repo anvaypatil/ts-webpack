8d7c946e966ba5eac167fd6ee7b95eae80a45812
Anvay P <anvay13@gmail.com>
2021-01-18 11:09:23 +0530

    array-1

---
+++ src/scripts/datastructures/array/array-one-liners.ts
@@ -0,0 +1,122 @@
+export namespace ArrayOneLiner {
+    // Check if an array is empty
+    export const isEmpty = arr => !Array.isArray(arr) || arr.length === 0;
+
+    // Clone an array
+    export const clone_1 = arr => arr.slice(0);
+    export const clone_2 = arr => [...arr];
+    export const clone_3 = arr => Array.from(arr);
+    export const clone_4 = arr => arr.map(x => x);
+    export const clone_5 = arr => JSON.parse(JSON.stringify(arr));
+    export const clone_6 = arr => arr.concat([]);
+
+    // Compare two arrays
+    export const isEqual = (a, b) => a.length === b.length && a.every((v, i) => v === b[i]);
+
+    // Convert an array of strings to numbers
+    export const toNumbers = (arr: Array<Number>) => arr.map(Number);
+
+    // Convert an array of numbers to strings
+    export const toStrings = (arr: Array<String>) => arr.map(String);
+
+    // Count by the properties of an array of objects
+    /**
+     countBy([
+     { branch: 'audi', model: 'q8', year: '2019' },
+     { branch: 'audi', model: 'rs7', year: '2020' },
+     { branch: 'ford', model: 'mustang', year: '2019' },
+     { branch: 'ford', model: 'explorer', year: '2020' },
+     { branch: 'bmw', model: 'x7', year: '2020' },
+     ], 'branch');
+     output: { 'audi': 2, 'ford': 2, 'bmw': 1 }
+     **/
+    export const countBy = (arr, prop) => arr.reduce((prev, curr) => (prev[curr[prop]] = ++prev[curr[prop]] || 1, prev), {});
+
+    // Count the occurrences of a value in an array
+    /**
+     countOccurrences([2, 1, 3, 3, 2, 3], 2);                // 2
+     countOccurrences(['a', 'b', 'a', 'c', 'a', 'b'], 'a');  // 3
+     */
+    export const countOccurrencesOf = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+
+    // Count the occurrences of array elements
+    /**
+     countOccurrences([2, 1, 3, 3, 2, 3]);               // { '1': 1, '2': 2, '3': 3 }
+     countOccurrences(['a', 'b', 'a', 'c', 'a', 'b']);   // { 'a': 3, 'b': 2, 'c': 1 }
+     **/
+    export const countOccurrences = arr => arr.reduce((prev, curr) => (prev[curr] = ++prev[curr] || 1, prev), {});
+
+    // array of numbers in given range
+    export const range_1 = (min, max) => [...Array(max - min + 1).keys()].map(i => i + min);
+    export const range_2 = (min, max) => Array(max - min + 1).fill(0).map((_, i) => min + i);
+    export const range_3 = (min, max) => Array.from({length: max - min + 1}, (_, i) => min + i);
+
+    // Empty an array
+    export const empty = arr => arr.length = 0;
+
+    // Find the number from `arr` which is closest to `n`
+    // closest([29, 87, 8, 78, 97, 20, 75, 33, 24, 17], 50);   // 33
+    export const closest_1 = (arr, n) => arr.reduce((prev, curr) => Math.abs(curr - n) < Math.abs(prev - n) ? curr : prev);
+    export const closest_2 = (arr, n) => arr.sort((a, b) => Math.abs(a - n) - Math.abs(b - n))[0];
+
+    // Maximum item from an array
+    export const max = arr => Math.max(...arr);
+
+    // Minimum item from an array
+    export const min = arr => Math.min(...arr);
+
+    // Flatten an array
+    export const flat_1 = arr => [].concat.apply([], arr.map(a => Array.isArray(a) ? flat_1(a) : a));
+    export const flat_2 = arr => arr.reduce((a, b) => Array.isArray(b) ? [...a, ...flat_2(b)] : [...a, b], []);
+
+    // Intersection of arrays
+    // @ts-ignore
+    export const intersection = <Type>(a: Array<Type>, ...arr: Array<Array<Type>>) => [...new Set(a)].filter((v: Type) => arr.every(<Type>(b: Array<Type>) => b.includes(v)))
+
+    // GroupBy a key
+    /**
+     groupBy([
+         { branch: 'audi', model: 'q8', year: '2019' },
+         { branch: 'audi', model: 'rs7', year: '2020' },
+         { branch: 'ford', model: 'mustang', year: '2019' },
+         { branch: 'ford', model: 'explorer', year: '2020' },
+         { branch: 'bmw', model: 'x7', year: '2020' },
+     ], 'branch');
+
+     /*
+     {
+        audi: [
+            { branch: 'audi', model: 'q8', year: '2019' },
+            { branch: 'audi', model: 'rs7', year: '2020' }
+        ],
+        bmw: [
+            { branch: 'bmw', model: 'x7', year: '2020' }
+        ],
+        ford: [
+            { branch: 'ford', model: 'mustang', year: '2019' },
+            { branch: 'ford', model: 'explorer', year: '2020' }
+        ],
+     }
+     **/
+    export const groupBy = (arr, key) => arr.reduce((acc, item) => ((acc[item[key]] = [...(acc[item[key]] || []), item]), acc), {});
+
+    // Partition an array based on a condition
+    /**
+      partition([1, 2, 3, 4, 5], n => n % 2);     // [[2, 4], [1, 3, 5]]
+     **/
+    export const partition = (arr, criteria) => arr.reduce((acc, i) => (acc[criteria(i) ? 0 : 1].push(i), acc), [[], []]);
+
+
+    // Zip multiple arrays
+    /**
+     zip(['a', 'b', 'c', 'd', 'e'], [1, 2, 3, 4, 5]);   // [['a', 1], ['b', 2], ['c', 3], ['d', 4], ['e', 5]]
+     **/
+    export const zip = (...arr) => Array.from({length: Math.max(...arr.map(a => a.length))}, (_, i) => arr.map(a => a[i]));
+
+    // Unzip an array of arrays
+    /**
+     unzip([['a', 1], ['b', 2], ['c', 3], ['d', 4], ['e', 5]]);  // [['a', 'b', 'c', 'd', 'e'], [1, 2, 3, 4, 5]]
+     **/
+    export const unzip = arr => arr.reduce((acc, c) => (c.forEach((v, i) => acc[i].push(v)), acc), Array.from({length: Math.max(...arr.map(a => a.length))}, (_) => []));
+
+}
---
+++ src/scripts/datastructures/array/array-test.ts
@@ -0,0 +1,112 @@
+export function array_functions() {
+    // https://www.w3schools.com/jsref/jsref_obj_array.asp
+    const arr1 = [1, 2, 3, 4, 5];
+    const arr2 = [6, 7, 8, 9];
+
+    // concat(): Joins two or more arrays, and returns a copy of the joined arrays
+    // array1.concat(array2, array3, ..., arrayX)
+    const result = arr1.concat(arr2);
+
+    // copyWithin(): Copies array elements within the array, to and from specified positions
+    // array.copyWithin(target, start, end)
+    const fruits = ["Banana", "Orange", "Apple", "Mango"];
+    fruits.copyWithin(2, 0); // Mutable operation
+    // ["Banana", "Orange", "Banana", "Orange"]
+
+    // entries(): Returns a key/value pair Array Iteration Object
+    for (obj of fruits.entries()) {
+        console.log(obj);
+    }
+    // (2) [0, "Banana"]
+    // (2) [1, "Orange"]
+    // (2) [2, "Banana"]
+    // (2) [3, "Orange"]
+
+    // every(): Checks if every element in an array pass a test
+    const ages = [32, 33, 16, 40];
+    const adults: boolean = ages.every((age) => age >= 18);
+
+    // fill(): Fill all the array elements with a static value:
+    const fruits = ["Banana", "Orange", "Apple", "Mango"];
+    fruits.fill("Kiwi");
+    // ["Kiwi", "Kiwi", "Kiwi", "Kiwi"]
+
+    // filter(): Creates a new array with every element in an array that pass a test
+    const adultsAges: Array = ages.filter((age) => age >= 18);
+
+    // find(): Returns the value of the first element in an array that pass a test
+    const firstAdult = ages.find((age) => age >= 18);
+
+    // findIndex(): Returns the index of the first element in an array that pass a test
+    const firstAdultIndex = ages.findIndex((age) => age >= 18);
+
+    // forEach(): Calls a function for each array element
+    fruits.forEach((fruit) => console.log(fruit));
+
+    // from(): Creates an array from an object
+    const alphabets = Array.from("ABCDEFG");
+
+    // flat an array
+    const flatArray = [1, 2, 3, 4, [6, 7, 8] [8, 9]].flat();
+
+    // includes(): Check if an array contains the specified element
+    const hasMango = fruits.includes("Mango"); // true
+
+    // indexOf(): Search the array for an element and returns its position
+    const appleIndex = fruits.indexOf("Apple");
+
+    // isArray(): Checks whether an object is an array
+    const arrayCheck = Array.isArray(fruits);
+
+    // join(): Joins all elements of an array into a string
+    const fruitsString = fruits.join(" | ");
+
+    // keys(): Returns a Array Iteration Object, containing the keys of the original array
+    const fk = fruits.keys();
+
+    // convert an Array Iterator object to Array
+    const keysArray = [...fk];
+
+    // lastIndexOf(): Search the array for an element, starting at the end, and returns its position
+    // array.lastIndexOf(item, start)
+    fruits.lastIndexOf("Apple");
+
+    // map(): Creates a new array with the result of calling a function for each array element
+    const numbers = [4, 9, 16, 25];
+    const square_roots = numbers.map(Math.sqrt);
+
+    // pop(): Removes the last element of an array, and returns that element
+    const fruit = fruits.pop();
+
+    // push(): Adds new elements to the end of an array, and returns the new length
+    fruits.push(fruit);
+
+    // reduce(): Reduce the values of an array to a single value (going left-to-right)
+    numbers.reduce((acc, num) => acc - num);
+
+    // reduceRight(): Reduce the values of an array to a single value (going right-to-left)
+    numbers.reduceRight((acc, num) => acc - num);
+
+    // reverse(): Reverses the order of the elements in an array
+    numbers.reverse();
+
+    // shift(): Removes the first element of an array, and returns that element
+    numbers.shift();
+
+    // slice(): Selects a part of an array, and returns the new array
+    fruits.slice(1, 3);
+
+    // some(): Checks if any of the elements in an array pass a test
+    const anyAdults: boolean = ages.some((age) => age >= 18);
+
+    // sort(): Sorts the elements of an array
+    const points = [40, 100, 1, 5, 25, 10];
+    points.sort((a, b) => a - b);
+
+    // splice(): Adds/Removes elements from an array
+    // array.splice(index, howmany, item1, ....., itemX)
+    fruits.splice(2, 0, "Lemon", "Kiwi"); // Mutable operation
+
+    // unshift(): Add new items to the beginning of an array
+    fruits.unshift("Lemon", "Pineapple"); // Mutable operation
+}
