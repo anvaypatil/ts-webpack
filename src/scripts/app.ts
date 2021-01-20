285072681c7b154b55e9bd0a8ec25d5ee67c9b40
Anvay P <anvay13@gmail.com>
2021-01-20 12:20:35 +0530

    added app code.

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -2,7 +2,7 @@ import '../styles/app.scss';
 
 import {test as testLinkedList} from "./datastructures/linked-list"
 import {oneLiners} from "./datastructures/primitives/one-liner"
-import {testFixedBitArray} from "./datastructures/bitarray/fixed-bit-array-test"
+import {testFixedBitArray} from "./datastructures/arrays/bitarray/fixed-bit-array-test"
 
 // testLinkedList();
 // oneLiners();
